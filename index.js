13a2379de9affd658b60163076ad381b552d3e9a
Tewodros Assefa <teddyassef23@gmail.com>
2024-01-02 16:58:09 -0600

    the main part is done

--- index.js
+++ index.js
@@ -1,87 +1,105 @@
 // Declaring the dependencies and variables
 const fs = require("fs");
-const util = require("util");
-const inquirer = require("inquirer");
-const generateReadme = require("./utils/generateReadme")
-const writeFileAsync = util.promisify(fs.writeFile);
+var inquirer = require('inquirer');
+const generateReadme = require("./generateReadme.js");
+const README  = './generat/readme.md';
 
 //Prompt the user questions to populate the README.md
-function promptUser(){
-    return inquirer.prompt([
-        {
-            type: "input",
-            name: "projectTitle",
-            message: "What is the project title?",
-        },
-        {
-            type: "input",
-            name: "description",
-            message: "Write a brief description of your project: "
-        },
-        {
-            type: "input",
-            name: "installation",
-            message: "Describe the installation process if any: ",
-        },
-        {
-            type: "input",
-            name: "usage",
-            message: "What is this project usage for?"
-        },
-        {
-            type: "list",
-            name: "license",
-            message: "Chose the appropriate license for this project: ",
-            choices: [
-                "Apache",
-                "Academic",
-                "GNU",
-                "ISC",
-                "MIT",
-                "Mozilla",
-                "Open"
-            ]
-        },
-        {
-            type: "input",
-            name: "contributing",
-            message: "Who are the contributors of this projects?"
-        },
-        {
-            type: "input",
-            name: "tests",
-            message: "Is there a test included?"
-        },
-        {
-            type: "input",
-            name: "questions",
-            message: "What do I do if I have an issue? "
-        },
-        {
-            type: "input",
-            name: "username",
-            message: "Please enter your GitHub username: "
-        },
-        {
-            type: "input",
-            name: "email",
-            message: "Please enter your email: "
-        }
-    ]);
-} 
-
-// Async function using util.promisify 
-  async function init() {
-    try {
-        // Ask user questions and generate responses
-        const answers = await promptUser();
-        const generateContent = generateReadme(answers);
-        // Write new README.md to dist directory
-        fs.writeFileAsync('./dist/README.md', generateContent);
-        console.log('✔️  Successfully wrote to README.md');
-    }   catch(err) {
-        console.log(err);
+var quistions = [
+    {
+        type: "input",
+        name: "projectTitle",
+        message: "What is the project title?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Write a brief description of your project: "
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "Describe the installation process if any: ",
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "What is this project usage for?"
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "Chose the appropriate license for this project: ",
+        choices: [
+            "Apache",
+            "Academic",
+            "GNU",
+            "ISC",
+            "MIT",
+            "Mozilla",
+            "Open"
+        ]
+    },
+    {
+        type: "input",
+        name: "contributing",
+        message: "Who are the contributors of this projects?"
+    },
+    {
+        type: "input",
+        name: "tests",
+        message: "Is there a test included?"
+    },
+    {
+        type: "input",
+        name: "questions",
+        message: "What do I do if I have an issue? "
+    },
+    {
+        type: "input",
+        name: "username",
+        message: "Please enter your GitHub username: "
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Please enter your email: "
     }
-  }
-  
-  init();  
+];
+
+
+
+
+// }
+
+function Creator() {
+
+    inquirer
+        .prompt(quistions)
+        .then(async (answers) => {
+            console.log(answers);
+            var newAnswer = generateReadme.generateMarkdown(answers);
+            if (fs.existsSync(README)){
+                fs.unlinkSync(README);
+            }
+                   
+    fs.writeFile('./generat/readme.md', newAnswer, function (error) {
+        if (error) {
+            return console.log(error)
+        }
+        console.log('success')
+
+    });
+        })
+        .catch((error) => {
+            if (error.isTtyError) {
+                
+            } else {
+              
+            }
+        });
+
+
+}
+
+Creator();
